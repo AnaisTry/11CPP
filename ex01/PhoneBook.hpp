16a6cbfd59ca39aedcf5ac9314ed5160b7fe38d1
angassin <angassin@student.s19.be>
2024-02-06 20:30:47 +0100

    Contact.hpp

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -3,22 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angassin <angassin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: angassin <angassin@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 22:53:25 by angassin          #+#    #+#             */
-/*   Updated: 2024/02/05 15:05:50 by angassin         ###   ########.fr       */
+/*   Updated: 2024/02/06 20:30:16 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
-
+# include "Contact.hpp"
 class PhoneBook 
 {
+	private:
+	
+		Contact contacts_[8];
+		int contactsNumber_;
+		int oldestContact_;
+
 	public:
 	
 		PhoneBook();
 		~PhoneBook();
+		void addContact(Contact contact);
+		Contact getContact(int index) const;
 };
 
 #endif
