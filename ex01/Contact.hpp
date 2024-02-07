710afea4b42557e6a507f688199201dd9926656f
AnaisTry <angassin@student.s19.be>
2024-02-07 13:57:07 +0100

    added default constructor

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -12,10 +12,16 @@ class Contact
 		std::string phoneNumber_;
 		std::string darkestSecret_;
 
+		//default constructor automatically initalized to empty strings 
+		//(non-class types need to be explicitly initiallized)
+		Contact(); 
+
 	public:
+		// constructor and destructor
 		Contact(std::string firstName, std::string lastName, std::string nickname,
 		 		std::string phoneNumber, std::string darkestSecret);
 		~Contact();
+		// getters
 		std::string getFirstName() const;
 		std::string getLastName() const;
 		std::string getNickname() const;
