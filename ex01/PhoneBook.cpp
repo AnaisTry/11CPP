567cbf651d92c563c9e2bb9db1014eca2eef7768
angassin <angassin@student.s19.be>
2024-02-07 14:24:27 +0100

    fixed typo

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,14 +6,14 @@
 /*   By: angassin <angassin@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 22:53:33 by angassin          #+#    #+#             */
-/*   Updated: 2024/02/03 22:59:56 by angassin         ###   ########.fr       */
+/*   Updated: 2024/02/07 14:14:16 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "PhoneBook.hpp"
 
-PhoneBook::PhoneBook()
+PhoneBook::PhoneBook(): contactsNumber_(0), oldestContact_(0)
 {
 	std::cout << "Phonebook created" << std::endl;
 } 
