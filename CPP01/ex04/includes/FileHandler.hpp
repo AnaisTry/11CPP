2d7a5015cc4115a1dba2415f7014c69d628e360b
angassin <angassin@student.s19.be>
2024-03-15 18:05:26 +0100

    rm unnecessary header, harl

--- CPP01/ex04/includes/FileHandler.hpp
+++ CPP01/ex04/includes/FileHandler.hpp
@@ -3,17 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   FileHandler.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angassin <angassin@student.s19.be>         +#+  +:+       +#+        */
+/*   By: angassin <angassin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 19:17:16 by angassin          #+#    #+#             */
-/*   Updated: 2024/03/14 13:12:25 by angassin         ###   ########.fr       */
+/*   Updated: 2024/03/15 18:03:17 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILEHANDLER_HPP
 # define FILEHANDLER_HPP
 
-# include <iostream>
 # include <fstream> // std::ifstream, std::ofstream
 # include <sstream> // std::stringstream 
 
