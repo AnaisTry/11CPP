813c752d7df87b856317771048074e030ef37a45
angassin <angassin@student.s19.be>
2024-04-04 16:17:58 +0200

    added default constructor

--- CPP03/ex00/srcs/ClapTrap.cpp
+++ CPP03/ex00/srcs/ClapTrap.cpp
@@ -3,15 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ClapTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angassin <angassin@student.s19.be>         +#+  +:+       +#+        */
+/*   By: angassin <angassin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 13:31:53 by angassin          #+#    #+#             */
-/*   Updated: 2024/04/03 14:14:59 by angassin         ###   ########.fr       */
+/*   Updated: 2024/04/04 16:17:16 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
+ClapTrap::ClapTrap():name_(""), hitPoints_(10), energyPoints_(10),
+	attackDamage_(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
 
 ClapTrap::ClapTrap(std::string name): name_(name), hitPoints_(10), energyPoints_(10),
 	attackDamage_(0)
@@ -19,8 +24,10 @@ ClapTrap::ClapTrap(std::string name): name_(name), hitPoints_(10), energyPoints_
 	std::cout << "ClapTrap " << this->name_ << " created" << std::endl;
 }
 
+ClapTrap::ClapTrap(std::string name)
 void ClapTrap::attack(const std::string &target)
 {
 	std::cout << "ClapTrap " << this->name_ << " attacks " << target
 	<< ", causing " << this->attackDamage_ << " points of damage!" << std::endl;
 }
+
