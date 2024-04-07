3e1f17ea4d910ae78ac68822aafd6b33507e3921
angassin <angassin@student.s19.be>
2024-04-07 19:11:35 +0200

    tested multiple objects

--- CPP03/ex01/srcs/main.cpp
+++ CPP03/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: angassin <angassin@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 17:07:43 by angassin          #+#    #+#             */
-/*   Updated: 2024/04/07 18:44:47 by angassin         ###   ########.fr       */
+/*   Updated: 2024/04/07 19:05:40 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,40 @@ int	main()
 	scavonymous.guardGate();
 
 	std::cout << std::endl;
+
+	// Create multiple ScavTrap objects
+    ScavTrap scav1("Scav1");
+	std::cout << std::endl;
+    ScavTrap scav2("Scav2");
+	std::cout << std::endl;
+    ScavTrap scav3("Scav3");
+	std::cout << std::endl;
+
+    // Test attacking with each ScavTrap
+    scav1.attack("Target1");
+    scav2.attack("Target2");
+    scav3.attack("Target3");
+	std::cout << std::endl;
+
+    // Test that the actions of one ScavTrap don't affect the others
+    scav1.takeDamage(20);
+    scav2.beRepaired(10);
+    scav3.guardGate();
+	std::cout << std::endl;
+
+    // Print out the state of each ScavTrap again to check if they've changed correctly
+    std::cout << scav1.getName() << " has " << scav1.getHitPoints() << " hitPoints, "
+              << scav1.getEnergyPoints() << " energyPoints, and "
+              << scav1.getDamage() << " attackDamage" << std::endl;
+
+    std::cout << scav2.getName() << " has " << scav2.getHitPoints() << " hitPoints, "
+              << scav2.getEnergyPoints() << " energyPoints, and "
+              << scav2.getDamage() << " attackDamage" << std::endl;
+
+    std::cout << scav3.getName() << " has " << scav3.getHitPoints() << " hitPoints, "
+              << scav3.getEnergyPoints() << " energyPoints, and "
+              << scav3.getDamage() << " attackDamage" << std::endl << std::endl;
+	
 
 	return 0;
 }
