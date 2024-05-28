5ec45d53dd1c1aed16016ed7c2d0f100f2bb0177
angassin <angassin@student.s19.be>
2024-05-28 20:35:32 +0200

    draft project and makefile recognizes when header files are changed

--- CPP07/ex00/includes/manip.hpp
+++ CPP07/ex00/includes/manip.hpp
@@ -6,7 +6,7 @@
 /*   By: angassin <angassin@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 20:10:22 by angassin          #+#    #+#             */
-/*   Updated: 2024/05/28 20:27:49 by angassin         ###   ########.fr       */
+/*   Updated: 2024/05/28 20:35:20 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ void swap(T &a, T &b)
 template <typename T>
 T min(T a, T b)
 {
-	std::cout << "template min" << std::endl;
 	return (a < b ? a : b);
 }
 
