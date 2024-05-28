e7a10692014fe947e23c4c69bf9ea4ea54548322
angassin <angassin@student.s19.be>
2024-05-28 22:01:32 +0200

    ex00 ok

--- CPP07/ex00/srcs/main.cpp
+++ CPP07/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: angassin <angassin@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 20:08:51 by angassin          #+#    #+#             */
-/*   Updated: 2024/05/28 20:15:19 by angassin         ###   ########.fr       */
+/*   Updated: 2024/05/28 22:00:07 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,17 @@ int main()
 {
 	int a = 2;
 	int b = 3;
+
 	::swap( a, b );
+	
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	// std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	// std::cout << "max( c, d ) = " << ::max( c, d << std;;endl;
+	std::cout << "max( c, d ) = " << ::max( c, d )<< std::endl;
 	return 0;
 }
