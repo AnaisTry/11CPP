1e3ddcd05d8a051149937b6b714cddc86c2be984
angassin <angassin@student.s19.be>
2024-07-02 11:39:24 +0200

    other limit cases

--- CPP08/ex00/srcs/main.cpp
+++ CPP08/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: angassin <angassin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 20:51:38 by angassin          #+#    #+#             */
-/*   Updated: 2024/07/02 11:22:09 by angassin         ###   ########.fr       */
+/*   Updated: 2024/07/02 11:37:26 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <list>
 #include <vector>
 #include <deque>
+#include <climits>
 
 
 #define GREEN   "\033[32m"
@@ -48,22 +49,46 @@ int main()
 
 	std::deque<int> deq;
 	searchContainer(deq, "deq", 5);
-	
-	std::cout << GREEN << "Test with number missing" << RESET << std::endl;
+
+	std::cout << GREEN << "Test container of one element that is not the one we look for" << RESET << std::endl;
 
 	lst.push_back(0);
-	lst.push_back(4);
-	lst.push_back(10);
 	searchContainer(lst, "lst", 5);
-	
+
 	myVector.push_back(0);
-	myVector.push_back(4);
-	myVector.push_back(10);
 	searchContainer(myVector, "myVector", 5);
 
 	deq.push_back(0);
-	deq.push_back(4);
-	deq.push_back(10);
+	searchContainer(deq, "deq", 5);
+
+	std::cout << GREEN << "Test container containing only the element we look for" << RESET << std::endl;
+
+	std::list<int> lst1;
+	lst1.push_back(5);
+	searchContainer(lst1, "lst1", 5);
+
+	std::vector<int> vector1;
+	vector1.push_back(5);
+	searchContainer(vector1, "vector1", 5);
+
+	std::deque<int> deq1;
+	deq1.push_back(5);
+	searchContainer(deq1, "deq1", 5);
+	
+	std::cout << GREEN << "Test with number missing" << RESET << std::endl;
+
+	lst.push_back(INT_MAX);
+	lst.push_back(INT_MIN);
+	searchContainer(lst, "lst", 5);
+	
+	
+	myVector.push_back(INT_MAX);
+	myVector.push_back(INT_MIN);
+	searchContainer(myVector, "myVector", 5);
+
+	
+	deq.push_back(INT_MAX);
+	deq.push_back(INT_MIN);
 	searchContainer(deq, "deq", 5);
 
 	std::cout << GREEN << "Test with number included" << RESET << std::endl;
