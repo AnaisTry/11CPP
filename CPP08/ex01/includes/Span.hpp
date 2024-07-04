bfa6422f4376fd165ba07a41c59787682371a8bb
angassin <angassin@student.s19.be>
2024-07-04 19:09:22 +0200

    addRange prototype

--- CPP08/ex01/includes/Span.hpp
+++ CPP08/ex01/includes/Span.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Span.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angassin <angassin@student.s19.be>         +#+  +:+       +#+        */
+/*   By: angassin <angassin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 12:20:10 by angassin          #+#    #+#             */
-/*   Updated: 2024/07/04 00:10:28 by angassin         ###   ########.fr       */
+/*   Updated: 2024/07/04 19:08:42 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ class Span
 		Span& operator=(const Span& other);
 
 		void addNumber(int number);
+		void addRange(std::multiset<int>::iterator begin, std::multiset<int>::iterator end);
 		void printContainer() const;
 		size_t shortestSpan() const;
 		size_t longestSpan() const;
