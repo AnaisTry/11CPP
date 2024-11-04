e0943ed1d996328c6e46675383046cb5f5c1b229
angassin <angassin@student.s19.be>
2024-11-04 17:09:50 +0100

    RPN ok

--- CPP09/ex01/includes/RPN.hpp
+++ CPP09/ex01/includes/RPN.hpp
@@ -3,30 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   RPN.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angassin <angassin@student.s19.be>         +#+  +:+       +#+        */
+/*   By: angassin <angassin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 14:10:26 by angassin          #+#    #+#             */
-/*   Updated: 2024/11/04 14:21:35 by angassin         ###   ########.fr       */
+/*   Updated: 2024/11/04 16:38:20 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RPN_HPP
 # define RPN_HPP
 # include <iostream>
+# include <stack>
+# include <sstream>
+# include <stdexcept>
+# include <cstdlib> // atof
 
 class RPN
 {
 	public:
+
 		RPN();
 		virtual ~RPN();
 		RPN(const RPN& other);
 		RPN& operator=(const RPN& other);
 
-	 protected:
-		// Protected members
+		double	evaluate(const std::string expression);
 
 	 private:
-		// Private members
+
+		std::stack<double>	stack_;
+		std::string			token_;
+
+		void	processToken();
+		void	handleOperator();
+		void	executeOperation(char mathOperator, double a, double b);
 };
 
 #endif
--- CPP09/ex01/srcs/RPN.cpp
+++ CPP09/ex01/srcs/RPN.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   RPN.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angassin <angassin@student.s19.be>         +#+  +:+       +#+        */
+/*   By: angassin <angassin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 14:30:33 by angassin          #+#    #+#             */
-/*   Updated: 2024/11/04 14:34:43 by angassin         ###   ########.fr       */
+/*   Updated: 2024/11/04 17:08:35 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,69 @@ RPN::RPN(const RPN& other)
 RPN& RPN::operator=(RPN const& other)
 {
 	// std::cout << "RPN assignment operator called" << std::endl;
-	if (this != &other) {
-		// Copy data members from other
+	if (this != &other) 
+	{
+		stack_ = other.stack_;
 	}
 	return *this;
+}
+
+double	RPN::evaluate(const std::string expression)
+{
+	std::istringstream	iss(expression);
+
+	while (iss >> token_)
+		processToken();
+	
+	if (stack_.size() != 1)
+		throw std::runtime_error("Error: invalid");
+	
+	return stack_.top();
+	
+}
+
+void	RPN::processToken()
+{
+	if (isdigit(token_[0]))
+		stack_.push(std::atof(token_.c_str()));	
+	else if (token_.length() == 1)
+		handleOperator();
+	else
+	{
+		throw std::runtime_error("Error: invalid token.");
+	}
+	
+}
+
+void	RPN::handleOperator()
+{
+	if (stack_.size() < 2)
+		throw std::runtime_error("Error: insufficient operands");
+	
+	double b = stack_.top(); stack_.pop();
+	double a = stack_.top(); stack_.pop();
+	executeOperation(token_[0], a, b);
+}
+
+void	RPN::executeOperation(char mathOperator, double a, double b)
+{
+	switch (mathOperator)
+	{
+		case '+':
+			stack_.push(a + b);
+			break;
+		case '-':
+			stack_.push(a - b);
+			break;
+		case '*':
+			stack_.push(a * b);
+			break;
+		case '/':
+			if (b == 0)
+				throw std::runtime_error("Error: division by zero.");
+			stack_.push(a / b);
+			break;
+		default:
+			throw std::runtime_error("Error: invalid operator.");
+	}
 }
