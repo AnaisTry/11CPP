1d0176a3cf1a151ef9c9d5548972591c689bf34f
angassin <angassin@student.s19.be>
2024-11-04 18:00:22 +0100

    skeletton PmergeMe

---
+++ CPP09/ex02/includes/PmergeMe.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angassin <angassin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 17:34:08 by angassin          #+#    #+#             */
+/*   Updated: 2024/11/04 17:59:45 by angassin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PmergeMe_HPP
+# define PmergeMe_HPP
+# include <iostream>
+# include <vector>
+# include <deque>
+# include <sstream>
+# include <stdexcept>
+# include <cstdlib> // atof
+
+class PmergeMe
+{
+	public:
+
+		PmergeMe();
+		virtual ~PmergeMe();
+		PmergeMe(const PmergeMe& other);
+		PmergeMe& operator=(const PmergeMe& other);
+
+
+	 private:
+
+		std::vector<int> integers_;
+		std::deque<int> temp_;
+
+};
+
+#endif
