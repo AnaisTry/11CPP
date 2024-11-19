29a6812eef4c360c8519caeafb137c7766c38449
angassin <angassin@student.s19.be>
2024-11-19 16:11:20 +0100

    draft PmergeMe.hpp

--- CPP09/ex02/includes/PmergeMe.hpp
+++ CPP09/ex02/includes/PmergeMe.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angassin <angassin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: angassin <angassin@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:34:08 by angassin          #+#    #+#             */
-/*   Updated: 2024/11/04 17:59:45 by angassin         ###   ########.fr       */
+/*   Updated: 2024/11/19 16:10:55 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,55 @@
 # include <sstream>
 # include <stdexcept>
 # include <cstdlib> // atof
+# include <ctime> // clock_t
 
 class PmergeMe
 {
 	public:
 
-		PmergeMe();
+		PmergeMe(int argc, char* argv[]);
 		virtual ~PmergeMe();
 		PmergeMe(const PmergeMe& other);
 		PmergeMe& operator=(const PmergeMe& other);
 
+		void sortVector();
+		void sortDeque();
+
+		// The double is used to return the time taken to sort the container in decimal
+		template <typename Container, typename SortFunction>
+		double measureSortTime(Container& numbers, SortFunction sortFunction)
+		{
+			std::clock_t start = std::clock();
+			// sortFunction(numbers);
+			(this->*sortFunction)(numbers);
+			std::clock_t end = std::clock();
+			return static_cast<double>(end - start) / CLOCKS_PER_SEC;
+		}
+
+		template <typename Container>
+		void	printContainer(Container& numbers)
+		{
+			for (typename Container::iterator it = numbers.begin(); it != numbers.end(); it++)
+				std::cout << *it << " ";
+			std::cout << std::endl;
+		}
+
+		template <typename Container>
+		void	printResult(const Container& numbers, const std::string& containerName, double timeTaken)
+		{
+			std::cout << containerName << " sorted in " << timeTaken << " seconds" << " :" << std::endl;
+			printContainer(numbers);
+		}
 
 	 private:
 
-		std::vector<int> integers_;
-		std::deque<int> temp_;
+		std::vector<int> nbVector_;
+		std::deque<int> nbDeque_;
+
+		void parseInput(int argc, char* argv[]);
+
+		template <typename Container>
+		void	mergeInsertionSort(Container& numbers){}
 
 };
 
